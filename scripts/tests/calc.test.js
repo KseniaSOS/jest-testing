8454594bea0e5046ed2aba7fb519e5c8ed87aac3
Ksenia Sossin <sossin.ksenia@gmail.com>
2023-09-19 12:31:06 +0000

    Add new addition tests

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,7 +5,15 @@ describe("Calculator", () => {
     describe("Addition function", () => {
         test("schould return 42 for 20 + 20", () => {
             expect(addition(20, 22)).toBe(42);
-        })
+        });
+        test("schould return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
+        test('adding floating point numbers', () => {
+            const value = 0.1 + 0.2;
+            //expect(value).toBe(0.3);           This won't work because of rounding error
+            expect(value).toBeCloseTo(0.3); // This works.
+          });
     });
     describe("Subtraction function", () => {
 
@@ -14,9 +22,6 @@ describe("Calculator", () => {
 
     });
     describe("Division function", () => {
-
-    });
-    describe("Addition function", () => {
 
     });
 })
